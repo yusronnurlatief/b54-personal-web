d8faf8ab7895f0f999de99419f972601513be363
SRONNICS <yusronnurlatief>
2024-04-14 10:55:18 +0700

    update date

--- assets/js/myProject.js
+++ assets/js/myProject.js
@@ -103,18 +103,6 @@ function addProject(event) {
   newData();
 }
 
-function getBase64Image(img) { 
-  var canvas = document.createElement("canvas"); 
-  canvas.width = img.width; 
-  canvas.height = img.height; 
-
-  var ctx = canvas.getContext("2d"); 
-  ctx.drawImage(img, 0, 0); 
-
-  var dataURL = canvas.toDataURL("image/png"); 
-
-  return dataURL.replace(/^data:image\/(png|jpg);base64,/, ""); 
-}
 
 function newData() {
 
